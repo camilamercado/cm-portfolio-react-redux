ff67e6d67d7dbe24a55f3fb9238acf811dddc1c1
camila mercado <camilamercado11@gmail.com>
2020-05-05 11:32:53 -0400

    add enlightenment wines entry

--- app/database/data.js
+++ app/database/data.js
@@ -47,6 +47,22 @@ const data = [
     text: ''
   },
   {
+    projectTitle: 'Enlightenment Wines Meadery',
+    id: 'enlightenment-wines-meadery',
+    link: 'https://enlightenmentwines.com/',
+    linkTitle: 'www.enlightenmentwines.com',
+    tags: ['Ecwid Ecommerce', 'HTML', 'Javascript', 'CSS'],
+    imageUrl: 'https://lh3.googleusercontent.com/proxy/2PZp5qA7fvAPtZSb6OhaXHI2TRoVo-pAI8xwMc_sRWs3APqDHFK6C_dcakNWei6o6cXKN_a2ohW5G9fj5ohCmQ5kNpMHhG9N7A3a5PJxd7eZIpwfXXYH8rvK3JW8UECloip6bkl09dnXvx8EXuSY32DknXh0kjBxHvSWRH-NuLc6lrITx6mO53CLK5HOptezyGo',
+    media: [
+      {
+        mediaSrc: '415171493',
+        video: true,
+        caption: 'Screen Recording of Enlightenment Wines Meadery, 2020',
+      },
+    ],
+    text: 'Straightforward webstore for Enlightent Wines Meadery'
+  },
+  {
     projectTitle: 'Arial All Typeface',
     id: 'risk-pool',
     link: 'https://www.canopycanopycanopy.com/issues/24/contents/accessible-by-default',
@@ -95,7 +111,7 @@ const data = [
     theme: 'dark',
     link: 'https://web.archive.org/web/20160308193522/http://software-studios.com/',
     linkTitle: 'web.archive.org/software-studios.com',
-    tags: ['HTML', 'Javascript', 'CSS'],
+    tags: ['React.js', 'SCSS'],
     imageUrl: 'https://lh3.googleusercontent.com/proxy/2PZp5qA7fvAPtZSb6OhaXHI2TRoVo-pAI8xwMc_sRWs3APqDHFK6C_dcakNWei6o6cXKN_a2ohW5G9fj5ohCmQ5kNpMHhG9N7A3a5PJxd7eZIpwfXXYH8rvK3JW8UECloip6bkl09dnXvx8EXuSY32DknXh0kjBxHvSWRH-NuLc6lrITx6mO53CLK5HOptezyGo',
     media: [
       {
